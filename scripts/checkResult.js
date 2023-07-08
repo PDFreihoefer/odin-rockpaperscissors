4ab7e9e550c62c0a643c29466a46fd1bd6ee5da8
PDFreihoefer <paul.freihoefer@gmail.com>
2023-07-08 12:50:31 -0700

    Initial commit

---
+++ scripts/checkResult.js
@@ -0,0 +1,20 @@
+// This function checks the result of the rock, paper, scissors game
+
+function checkResult(playerChoice, computerChoice) {
+  // Computer choice is always uppercase, but we want to edit the player choice to ensure that
+  // we can support any spelling: 'rOck', 'Rock', 'ROCk'. JS is case-sensitive
+  
+  // Handle all ties immediately
+  if (playerChoice.toUpperCase == computerChoice) {
+    return 'tie'
+  }
+
+  switch (playerChoice.toUpperCase) {
+    case 'ROCK':
+      return computerChoice == 'SCISSORS' ? 'playerWin' : 'computerWin';
+    case 'PAPER':
+      return computerChoice == 'ROCK' ? 'playerWin' : 'computerWin';
+    case 'SCISSORS':
+      return computerChoice == 'PAPER' ? 'playerWin' : 'computerWin';
+  }
+}
